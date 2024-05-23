9f52e80a70fffa97eff498996bb64090ba848579
weronika_wolek <95313435+weronikawolek@users.noreply.github.com>
2024-05-23 02:39:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def initialize_tb_client():
 def fetch_and_process_data(tb_client, pipe_name, time_column, sum_column):
     response = tb_client.pipe(pipe_name).query()
     data = pd.json_normalize(response.data)
-    data['minuta'] = pd.to_datetime(data[time_column])
-    data = data.sort_values(by=['minuta'])
+    data[time_column] = pd.to_datetime(data[time_column])
+    data = data.sort_values(by=[time_column])
     data['suma_skumulowana'] = data.groupby('placowka')[sum_column].cumsum()
     return data
 
@@ -52,14 +52,31 @@ def fetch_and_process_procedures(tb_client, pipe_name):
     data = data.sort_values(by=['minuta'])
     return data
 
+# Pobieranie i przetwarzanie danych o średnim czasie wizyty
+def fetch_and_process_avg_visit_time(tb_client, pipe_name):
+    response = tb_client.pipe(pipe_name).query()
+    data = pd.json_normalize(response.data)
+    data['minuta'] = pd.to_datetime(data['minuta'])
+    data = data.sort_values(by=['minuta'])
+    return data
+
+# Pobieranie i przetwarzanie danych o dostępnym sprzęcie medycznym
+def fetch_and_process_medical_equipment(tb_client, pipe_name):
+    response = tb_client.pipe(pipe_name).query()
+    data = pd.json_normalize(response.data)
+    data['minuta'] = pd.to_datetime(data['minuta'])
+    data = data.sort_values(by=['minuta'])
+    return data
+
 app.layout = dbc.Container([
     dbc.Row([
-        dbc.Col(html.H1("Wybierz placówkę", className='text-center'), width=12),
+        dbc.Col(html.H1("Dostęp do usług medycznych uaktualnianych w czasie rzeczywistym", style={'color': 'LightBlue', 'font-family': 'Georgia', 'font-weight': 'bold'}, className='text-center'), width=12),
         dbc.Col(dcc.Dropdown(
             id='placowka-dropdown',
             options=[],  # Opcje będą zaktualizowane po inicjalnym załadowaniu danych
             value=None,  # Domyślna wartość
             placeholder="Wybierz placówkę",
+            style={'color': 'DarkPink', 'font-family': 'Georgia'},
             clearable=True
         ), width=12),
     ]),
@@ -68,43 +85,27 @@ app.layout = dbc.Container([
         interval=1200000,  # Co 20 minut
         n_intervals=0
     ),
+    dbc.Row([dbc.Col([dcc.Graph(id='bar-chart-pacjenci', config={'displayModeBar': False})], width=12)]),
+    dbc.Row([dbc.Col([dcc.Graph(id='bar-chart-nagle-przypadki', config={'displayModeBar': False})], width=12)]),  # Zmieniony wykres
+    dbc.Row([dbc.Col([dcc.Graph(id='pie-chart-operacje', config={'displayModeBar': False})], width=6, style={'min-height': '400px', 'padding': '10px'}),
+             dbc.Col([dcc.Graph(id='pie-chart-zabiegi', config={'displayModeBar': False})], width=6, style={'min-height': '400px', 'padding': '10px'})]),
     dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='bar-chart-operacje', config={'displayModeBar': True})
-        ], width=12)
+        dbc.Col([dcc.Graph(id='line-chart-hospitalizacja', config={'displayModeBar': False})], width=6),  # Wykres liniowy
+        dbc.Col([dcc.Graph(id='line-chart-czas-wizyty', config={'displayModeBar': False})], width=6)  # Wykres liniowy
     ]),
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='pie-chart-pacjenci', config={'displayModeBar': True}),
-        ], width=6, style={'min-height': '400px', 'padding': '10px'}),
-        dbc.Col([
-            dcc.Graph(id='pie-chart-zabiegi', config={'displayModeBar': True}),
-        ], width=6, style={'min-height': '400px', 'padding': '10px'})
-    ]),
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='scatter-chart-nagle-przypadki', config={'displayModeBar': True})
-        ], width=12)
-    ]),
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='bar-chart-hospitalizacja', config={'displayModeBar': True})
-        ], width=12)
-    ]),
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='line-chart-oczekiwanie', config={'displayModeBar': True})
-        ], width=12)
-    ])
+    dbc.Row([dbc.Col([dcc.Graph(id='line-chart-oczekiwanie', config={'displayModeBar': False})], width=12)]),  # Przesunięty wykres
+    dbc.Row([dbc.Col([dcc.Graph(id='bar-chart-sprzet', config={'displayModeBar': False})], width=12)])  # Nowy wykres słupkowy
 ], fluid=True)
 
 @app.callback(
-    [Output('bar-chart-operacje', 'figure'),
-     Output('pie-chart-pacjenci', 'figure'),
+    [Output('bar-chart-pacjenci', 'figure'),
+     Output('bar-chart-nagle-przypadki', 'figure'),  # Zmieniony wykres
+     Output('pie-chart-operacje', 'figure'),
      Output('pie-chart-zabiegi', 'figure'),
-     Output('scatter-chart-nagle-przypadki', 'figure'),
-     Output('bar-chart-hospitalizacja', 'figure'),
+     Output('line-chart-hospitalizacja', 'figure'),  # Wykres liniowy
+     Output('line-chart-czas-wizyty', 'figure'),  # Wykres liniowy
      Output('line-chart-oczekiwanie', 'figure'),
+     Output('bar-chart-sprzet', 'figure'),  # Nowy wykres słupkowy
      Output('placowka-dropdown', 'options')],
     [Input('interval-component', 'n_intervals'),
      Input('placowka-dropdown', 'value')]
@@ -114,19 +115,23 @@ def update_charts(n_intervals, selected_placowka):
 
     # Użycie concurrent.futures do równoległego pobierania danych
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_operacje = executor.submit(fetch_and_process_data, tb_client, 'liczba_operacji', 'minuta', 'suma_przeprowadzonych_operacji')
         future_pacjenci = executor.submit(fetch_and_process_data, tb_client, 'liczba_pacjentow', 'minuta', 'suma_przyjetych_pacjentow')
+        future_operacje = executor.submit(fetch_and_process_data, tb_client, 'liczba_operacji', 'minuta', 'suma_przeprowadzonych_operacji')
         future_nagle_przypadki = executor.submit(fetch_and_process_data, tb_client, 'liczba_naglych_przypadkow', 'minuta', 'suma_naglych_przypadkow')
         future_hospitalizacja = executor.submit(fetch_and_process_avg_hospitalization, tb_client, 'sredni_czas_hospitalizacji')
         future_oczekiwanie = executor.submit(fetch_and_process_avg_waiting_time, tb_client, 'sredni_czas_oczekiwania_na_wizyte')
         future_zabiegi = executor.submit(fetch_and_process_procedures, tb_client, 'liczba_wykonanych_zabiegow')
+        future_czas_wizyty = executor.submit(fetch_and_process_avg_visit_time, tb_client, 'sredni_czas_wizyty')
+        future_sprzet = executor.submit(fetch_and_process_medical_equipment, tb_client, 'sprzet_medyczny')
 
-        data_operacje = future_operacje.result()
         data_pacjenci = future_pacjenci.result()
+        data_operacje = future_operacje.result()
         data_nagle_przypadki = future_nagle_przypadki.result()
         data_hospitalizacja = future_hospitalizacja.result()
         data_oczekiwanie = future_oczekiwanie.result()
         data_zabiegi = future_zabiegi.result()
+        data_czas_wizyty = future_czas_wizyty.result()
+        data_sprzet = future_sprzet.result()
 
     # Aktualizacja opcji dropdown
     placowki = data_operacje['placowka'].unique()
@@ -134,55 +139,80 @@ def update_charts(n_intervals, selected_placowka):
 
     # Filtrowanie danych na podstawie wybranej placówki
     if selected_placowka:
-        filtered_operacje = data_operacje[data_operacje['placowka'] == selected_placowka]
         filtered_pacjenci = data_pacjenci[data_pacjenci['placowka'] == selected_placowka]
+        filtered_operacje = data_operacje[data_operacje['placowka'] == selected_placowka]
         filtered_nagle_przypadki = data_nagle_przypadki[data_nagle_przypadki['placowka'] == selected_placowka]
         filtered_hospitalizacja = data_hospitalizacja[data_hospitalizacja['placowka'] == selected_placowka]
         filtered_oczekiwanie = data_oczekiwanie[data_oczekiwanie['placowka'] == selected_placowka]
         filtered_zabiegi = data_zabiegi[data_zabiegi['placowka'] == selected_placowka]
+        filtered_czas_wizyty = data_czas_wizyty[data_czas_wizyty['placowka'] == selected_placowka]
+        filtered_sprzet = data_sprzet[data_sprzet['placowka'] == selected_placowka]
     else:
-        filtered_operacje = data_operacje
         filtered_pacjenci = data_pacjenci
+        filtered_operacje = data_operacje
         filtered_nagle_przypadki = data_nagle_przypadki
         filtered_hospitalizacja = data_hospitalizacja
         filtered_oczekiwanie = data_oczekiwanie
         filtered_zabiegi = data_zabiegi
+        filtered_czas_wizyty = data_czas_wizyty
+        filtered_sprzet = data_sprzet
 
     # Przygotowanie danych do wykresów
-    bar_data = filtered_operacje.groupby('placowka')['suma_przeprowadzonych_operacji'].sum().reset_index()
+    bar_data = filtered_pacjenci.groupby('placowka')['suma_przyjetych_pacjentow'].sum().reset_index()
 
-    fig_operacje = px.bar(
+    fig_pacjenci = px.bar(
         bar_data,
         x='placowka',
-        y='suma_przeprowadzonych_operacji',
-        labels={'suma_przeprowadzonych_operacji': 'Łączna liczba operacji', 'placowka': 'Placówka'},
+        y='suma_przyjetych_pacjentow',
+        labels={'suma_przyjetych_pacjentow': 'Łączna liczba pacjentów', 'placowka': 'Placówka'},
         template='plotly_white',
-        color_discrete_sequence=px.colors.qualitative.Plotly  # Niestandardowa sekwencja kolorów
+        color_discrete_sequence=px.colors.qualitative.Pastel1  # Niestandardowa sekwencja kolorów
     )
 
-    fig_operacje.update_layout(
-        title={'text': 'Liczba przeprowadzonych operacji w klinikach medycznych', 'x': 0.5, 'xanchor': 'center'},
+    fig_pacjenci.update_layout(
+        title={'text': 'Liczba przyjętych pacjentów w klinikach medycznych', 'x': 0.5, 'xanchor': 'center'},
         xaxis_title='Placówka',
-        yaxis_title='Łączna liczba operacji',
+        yaxis_title='Łączna liczba pacjentów',
         legend_title='Placówka',
         hovermode='x'
     )
 
-    # Przygotowanie danych do wykresu kołowego dla pacjentów
-    pie_data = filtered_pacjenci.groupby('placowka')['suma_skumulowana'].sum().reset_index()
+    # Przygotowanie danych do wykresu poziomego słupkowego dla nagłych przypadków
+    bar_nagle_przypadki_data = filtered_nagle_przypadki.groupby('placowka')['suma_naglych_przypadkow'].sum().reset_index()
 
-    fig_pacjenci = px.pie(
+    fig_nagle_przypadki = px.bar(
+        bar_nagle_przypadki_data,
+        y='placowka',
+        x='suma_naglych_przypadkow',
+        labels={'suma_naglych_przypadkow': 'Liczba nagłych przypadków', 'placowka': 'Placówka'},
+        template='plotly_white',
+        orientation='h',  # Ustawienie orientacji wykresu na poziomą
+        color_discrete_sequence=px.colors.qualitative.Pastel2  # Niestandardowa sekwencja kolorów
+    )
+
+    fig_nagle_przypadki.update_layout(
+        title={'text': 'Liczba nagłych przypadków w klinikach medycznych', 'x': 0.5, 'xanchor': 'center', 'font': {'family': 'Georgia'}},
+        xaxis_title='Liczba nagłych przypadków',
+        yaxis_title='Placówka',
+        legend_title='Placówka',
+        hovermode='y'
+    )
+
+    # Przygotowanie danych do wykresu kołowego dla operacji
+    pie_data = filtered_operacje.groupby('placowka')['suma_skumulowana'].sum().reset_index()
+
+    fig_operacje = px.pie(
         pie_data,
         names='placowka',
         values='suma_skumulowana',
         template='plotly_white',
-        color_discrete_sequence=px.colors.qualitative.Set1  # Niestandardowa sekwencja kolorów
+        color_discrete_sequence=px.colors.qualitative.Pastel2  # Niestandardowa sekwencja kolorów
     )
 
-    fig_pacjenci.update_traces(textinfo='value')  # Wyświetlanie tylko liczby
+    fig_operacje.update_traces(textinfo='value')  # Wyświetlanie tylko liczby
 
-    fig_pacjenci.update_layout(
-        title={'text': 'Liczba przyjętych pacjentów w klinikach medycznych', 'x': 0.5, 'xanchor': 'center'}
+    fig_operacje.update_layout(
+        title={'text': 'Liczba przeprowadzonych operacji w klinikach medycznych', 'x': 0.5, 'xanchor': 'center', 'font': {'family': 'Georgia'}}
     )
 
     # Przygotowanie danych do wykresu kołowego dla zabiegów
@@ -193,56 +223,56 @@ def update_charts(n_intervals, selected_placowka):
         names='placowka',
         values='suma_wykonanych_zabiegow',
         template='plotly_white',
-        color_discrete_sequence=px.colors.qualitative.Plotly # Niestandardowa sekwencja kolorów
+        color_discrete_sequence=px.colors.qualitative.Pastel1 # Niestandardowa sekwencja kolorów
     )
 
     fig_zabiegi.update_traces(textinfo='value')  # Wyświetlanie tylko liczby
 
     fig_zabiegi.update_layout(
-        title={'text': 'Liczba wykonanych zabiegów w klinikach medycznych', 'x': 0.5, 'xanchor': 'center'}
+        title={'text': 'Liczba wykonanych zabiegów w klinikach medycznych', 'x': 0.5, 'xanchor': 'center', 'font': {'family': 'Georgia'}}
     )
 
-    # Przygotowanie danych do wykresu punktowego dla nagłych wypadków
-    scatter_data = filtered_nagle_przypadki.groupby(['minuta', 'placowka'])[
-        'suma_naglych_przypadkow'].sum().reset_index()
+    # Przygotowanie danych do wykresu liniowego dla średniego czasu hospitalizacji
+    line_hospitalizacja_data = filtered_hospitalizacja.groupby(['minuta', 'placowka'])[
+        'sredni_czas_hospitalizacji'].mean().reset_index()
 
-    fig_nagle_przypadki = px.scatter(
-        scatter_data,
+    fig_hospitalizacja = px.line(
+        line_hospitalizacja_data,
         x='minuta',
-        y='suma_naglych_przypadkow',
+        y='sredni_czas_hospitalizacji',
         color='placowka',
-        labels={'suma_naglych_przypadkow': 'Liczba nagłych wypadków', 'minuta': 'Minuta'},
+        labels={'sredni_czas_hospitalizacji': 'Średni czas hospitalizacji (minuty)', 'minuta': 'Czas'},
         template='plotly_white',
-        color_discrete_sequence=px.colors.qualitative.Dark2  # Niestandardowa sekwencja kolorów
+        color_discrete_sequence=px.colors.qualitative.Pastel1  # Niestandardowa sekwencja kolorów
     )
 
-    fig_nagle_przypadki.update_traces(mode='lines+markers')
-
-    fig_nagle_przypadki.update_layout(
-        title={'text': 'Liczba nagłych wypadków w klinikach medycznych', 'x': 0.5, 'xanchor': 'center'},
+    fig_hospitalizacja.update_layout(
+        title={'text': 'Średni czas hospitalizacji w klinikach medycznych', 'x': 0.5, 'xanchor': 'center', 'font': {'family': 'Georgia'}},
         xaxis_title='Czas',
-        yaxis_title='Liczba nagłych wypadków',
+        yaxis_title='Średni czas hospitalizacji (minuty)',
         legend_title='Placówka',
         hovermode='x'
     )
 
-    # Przygotowanie danych do wykresu słupkowego dla średniego czasu hospitalizacji
-    heatmap_data = filtered_hospitalizacja.pivot(index='minuta', columns='placowka',
-                                                 values='sredni_czas_hospitalizacji')
+    # Przygotowanie danych do wykresu liniowego dla średniego czasu wizyty
+    line_czas_wizyty_data = filtered_czas_wizyty.groupby(['minuta', 'placowka'])[
+        'sredni_czas_wizyty'].mean().reset_index()
 
-    bar_hospitalizacja = px.bar(
-        heatmap_data.transpose(),  # Transponowanie danych dla poprawnej orientacji
-        barmode='group',
-        labels={'value': 'Średni czas hospitalizacji (minuty)', 'index': 'Placówka', 'variable': 'Czas'},
+    fig_czas_wizyty = px.line(
+        line_czas_wizyty_data,
+        x='minuta',
+        y='sredni_czas_wizyty',
+        color='placowka',
+        labels={'sredni_czas_wizyty': 'Średni czas wizyty (minuty)', 'minuta': 'Czas'},
         template='plotly_white',
-        color_discrete_sequence=px.colors.qualitative.Prism  # Niestandardowa sekwencja kolorów
+        color_discrete_sequence=px.colors.qualitative.Pastel2  # Niestandardowa sekwencja kolorów
     )
 
-    bar_hospitalizacja.update_layout(
-        title={'text': 'Średni czas hospitalizacji w klinikach medycznych', 'x': 0.5, 'xanchor': 'center'},
-        xaxis_title='Placówka',
-        yaxis_title='Średni czas hospitalizacji (minuty)',
-        legend_title='Czas',
+    fig_czas_wizyty.update_layout(
+        title={'text': 'Średni czas wizyty w klinikach medycznych', 'x': 0.5, 'xanchor': 'center', 'font': {'family': 'Georgia'}},
+        xaxis_title='Czas',
+        yaxis_title='Średni czas wizyty (minuty)',
+        legend_title='Placówka',
         hovermode='x'
     )
 
@@ -250,25 +280,45 @@ def update_charts(n_intervals, selected_placowka):
     line_data = filtered_oczekiwanie.groupby(['minuta', 'placowka'])[
         'sredni_czas_oczekiwania_na_wizyte'].mean().reset_index()
 
-    scatter_oczekiwanie = px.scatter(
+    fig_oczekiwanie = px.line(
         line_data,
         x='minuta',
         y='sredni_czas_oczekiwania_na_wizyte',
         color='placowka',
         labels={'sredni_czas_oczekiwania_na_wizyte': 'Średni czas oczekiwania (minuty)', 'minuta': 'Czas'},
         template='plotly_white',
-        color_discrete_sequence=px.colors.qualitative.Set3  # Niestandardowa sekwencja kolorów
+        color_discrete_sequence=px.colors.qualitative.Pastel2  # Niestandardowa sekwencja kolorów
     )
 
-    scatter_oczekiwanie.update_layout(
-        title={'text': 'Średni czas oczekiwania na wizytę w klinikach medycznych', 'x': 0.5, 'xanchor': 'center'},
+    fig_oczekiwanie.update_layout(
+        title={'text': 'Średni czas oczekiwania na wizytę w klinikach medycznych', 'x': 0.5, 'xanchor': 'center', 'font': {'family': 'Georgia'}},
         xaxis_title='Czas',
         yaxis_title='Średni czas oczekiwania (minuty)',
         legend_title='Placówka',
         hovermode='x'
     )
 
-    return fig_operacje, fig_pacjenci, fig_zabiegi, fig_nagle_przypadki, bar_hospitalizacja, scatter_oczekiwanie, dropdown_options
+    # Przygotowanie danych do wykresu słupkowego dla dostępnego sprzętu medycznego
+    bar_sprzet_data = filtered_sprzet.groupby('placowka')['dostepny_sprzet_medyczny'].sum().reset_index()
+
+    fig_sprzet = px.bar(
+        bar_sprzet_data,
+        x='placowka',
+        y='dostepny_sprzet_medyczny',
+        labels={'dostepny_sprzet_medyczny': 'Dostępny sprzęt medyczny', 'placowka': 'Placówka'},
+        template='plotly_white',
+        color_discrete_sequence=px.colors.qualitative.Pastel1  # Niestandardowa sekwencja kolorów
+    )
+
+    fig_sprzet.update_layout(
+        title={'text': 'Dostępny sprzęt medyczny w klinikach medycznych', 'x': 0.5, 'xanchor': 'center', 'font': {'family': 'Georgia'}},
+        xaxis_title='Placówka',
+        yaxis_title='Dostępny sprzęt medyczny',
+        legend_title='Placówka',
+        hovermode='x'
+    )
+
+    return fig_pacjenci, fig_nagle_przypadki, fig_operacje, fig_zabiegi, fig_hospitalizacja, fig_czas_wizyty, fig_oczekiwanie, fig_sprzet, dropdown_options
 
 if __name__ == '__main__':
     app.run_server(debug=True)
